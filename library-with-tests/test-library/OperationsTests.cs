c6b75c58060255b63619f025e9a087ce8d53947b
Abhishek Kumar <ak@Abhisheks-MacBook-Pro.local>
2020-02-07 21:13:04 -0800

    Update a test

--- library-with-tests/test-library/OperationsTests.cs
+++ library-with-tests/test-library/OperationsTests.cs
@@ -8,9 +8,15 @@ namespace TestLibrary
     public class OperationsTests
     {
         [Fact]
-        public void TestAdd()
+        public void TestAddEquals()
         {
             Assert.Equal(42, new Operations().Add(19, 23));
+        }
+
+        [Fact]
+        public void TestAddNotEquals()
+        {
+            Assert.NotEqual(42, new Operations().Add(19, 20));
         }
     }
 }
